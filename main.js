0968204e236547078a207ce994d6628619c3d1c9
David Zou <davidzsy20@Davids-MacBook-Pro.local>
2018-04-27 18:15:32 -0400

    final brush up

--- main.js
+++ main.js
@@ -1,18 +1,53 @@
 // var colors = ["rgb(255, 0, 0)","rgb(25, 20, 40)","rgb(25, 20, 220)","rgb(55, 20, 20)","rgb(5, 4, 3)","rgb(2, 224, 40)"]
-var colors = generateRandomColors(6);
+var numSquares = 6
+var colors = generateRandomColors(numSquares);
 var squares = document.querySelectorAll(".squares");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#msg");
 var resetButton = document.querySelector('#reset');
 var h1 = document.querySelector("h1");
+var easyBtn = document.querySelector('#easy');
+var hardBtn = document.querySelector('#hard');
+
+easyBtn.addEventListener('click', function() {
+  hardBtn.classList.remove('selected')
+  easyBtn.classList.add("selected")
+  numSquares = 3;
+  colors = generateRandomColors(numSquares);
+  pickedColor = pickColor();
+
+  colorDisplay.textContent = pickedColor
+  for(var i = 0 ; i<squares.length; i++) {
+      if(colors[i]) {
+          squares[i].style.backgroundColor = colors[i];
+      }  else{
+          squares[i].style.display = 'none'
+      }
+  }
+})
+
+hardBtn.addEventListener('click', function() {
+    easyBtn.classList.remove("selected")
+    hardBtn.classList.add("selected")
+    numSquares = 6;
+    colors = generateRandomColors(numSquares);
+   pickedColor = pickColor();
+    for(var i = 0 ; i<squares.length; i++) {
+            squares[i].style.backgroundColor = colors[i];
+            squares[i].style.display = 'inline-block'
+    }
+    
+})
 resetButton.addEventListener("click", function() {
-   colors = generateRandomColors(6)
+   colors = generateRandomColors(numSquares)
 
    pickedColor = pickColor();
    colorDisplay.textContent = pickedColor;
+   messageDisplay.textContent = "";
    refresh()
    h1.style.backgroundColor = '#232323'
+   this.textContent = 'New Colors'
 })
 
 
